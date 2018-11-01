655e351758805582b2b363aacda2754ec6483603
nguye <nguyenv2@outlook.com>
2018-11-01 10:46:01 -0700

    scaffolding

---
+++ API/Controllers/ToDoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+  [Route("api/[controller]")]
+  public class ToDoController : ControllerBase
+  {
+    private readonly ToDoContext _context;
+    public ToDoController(ToDoContext context)
+    {
+      _context = context;
+      //Ensures the data colection is never emptu
+      if (_context.ToDoItems.Count() == 0)
+      {
+        _context.ToDoItems.Add(new Models.ToDoItem { Name = "Dummy Item" });
+        _context.SaveChanges();
+      }
+    }
+    [HttpGet]
+    // GET: api/<controller>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "value1", "value2" };
+    }
+
+    // GET api/<controller>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+      return "value";
+    }
+
+    // POST api/<controller>
+    [HttpPost]
+    public void Post([FromBody]string value)
+    {
+    }
+
+    // PUT api/<controller>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+  }
+}
---
+++ API/Data/ToDoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+  public class ToDoContext: DbContext
+  {
+    public ToDoContext(DbContextOptions<ToDoContext> options)
+      : base(options)
+    {
+    }
+
+    public DbSet<ToDoItem> ToDoItems { set; get; }
+  }
+}
